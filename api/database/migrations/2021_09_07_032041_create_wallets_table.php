25f75d709d5926bdc1da3aceec586f7bc628b1e8
santivarano <34697066+santivarano@users.noreply.github.com>
2021-09-06 23:05:27 -0500

    cc

---
+++ api/database/migrations/2021_09_07_032041_create_wallets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('type_transaction_id');
+            $table->string('value');
+            $table->string('description');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('type_transaction_id')->references('id')->on('type_transactions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
--- readme.txt
+++ readme.txt
@@ -2,3 +2,5 @@ https://dev.to/joselfonseca/graphql-auth-with-passport-and-lighthouse-php-14g5
 
 https://stackoverflow.com/questions/57582140/laravel-and-lighthouse-it-always-read-the-dafault-schema
 
+https://medium.com/@sadnub/api-auth-and-graphql-in-laravel-d191e277560d
+
